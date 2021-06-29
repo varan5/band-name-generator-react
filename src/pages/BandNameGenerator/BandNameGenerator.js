b8b76cd13098109c0f3683ca9ad89bae61b873c2
varan5 <varan2.bhalla@gmail.com>
2021-06-29 18:04:59 +0530

    Added band image

--- src/pages/BandNameGenerator/BandNameGenerator.js
+++ src/pages/BandNameGenerator/BandNameGenerator.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BandImage from '../../assets/images/band.png'
 
 const BandNameGenerator = () => {
+  const [cityName, setCityName] = useState('')
+  const [petsName, setPetsName] = useState('')
+
   return (
     <div>
-      <h1>Band Name Generator</h1>
+      <h3>Band Name Generator</h3>
+      {cityName}
+      {petsName}
+      <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s6 input-container">
+              <input
+                className="city-name-input"
+                autoFocus
+                placeholder="Enter your birth city name "
+                value={cityName}
+                onChange={(event) => setCityName(event.target.value)}
+              />
+              <input
+                className="pets-name-input"
+                placeholder="Enter your pet's name "
+                value={petsName}
+                onChange={(event) => setPetsName(event.target.value)}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+      <img src={BandImage} alt="" />
     </div>
   )
 }
